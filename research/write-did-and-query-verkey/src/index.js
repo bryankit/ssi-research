42c7ae851acf5c58392f8e3b82111b3f7adf33fb
ghabxph <11975597+ghabxph@users.noreply.github.com>
2020-10-28 08:42:09 +0800

    refactor(research/write-did): just added semicolon to silence the emacs linter

--- research/write-did-and-query-verkey/src/index.js
+++ research/write-did-and-query-verkey/src/index.js
@@ -12,45 +12,45 @@
  * would be used and DIDs would be exchanged using some channel of communication
  */
 
-const indy = require('indy-sdk')
-const util = require('./util')
-const colors = require('./colors')
+const indy = require('indy-sdk');
+const util = require('./util');
+const colors = require('./colors');
 
-const log = console.log
+const log = console.log;
 
 function logValue() {
-    log(colors.CYAN, ...arguments, colors.NONE)
+    log(colors.CYAN, ...arguments, colors.NONE);
 }
 
 async function run() {
 
-    log("Set protocol version 2 to work with Indy Node 1.4")
-    await indy.setProtocolVersion(2)
+    log("Set protocol version 2 to work with Indy Node 1.4");
+    await indy.setProtocolVersion(2);
 
     // Tell SDK which pool you are going to use. You should have already started
     // this pool using docker compose or similar. Here, we are dumping the config
     // just for demonstration purposes.
 
     // 1.
-    log('1. Creates a new local pool ledger configuration that is used later when connecting to ledger.')
-    const poolName = 'pool'
-    const genesisFilePath = await util.getPoolGenesisTxnPath(poolName)
-    const poolConfig = {'genesis_txn': genesisFilePath}
-    await indy.createPoolLedgerConfig(poolName, poolConfig)
+    log('1. Creates a new local pool ledger configuration that is used later when connecting to ledger.');
+    const poolName = 'pool';
+    const genesisFilePath = await util.getPoolGenesisTxnPath(poolName);
+    const poolConfig = {'genesis_txn': genesisFilePath};
+    await indy.createPoolLedgerConfig(poolName, poolConfig);
 
     // 2.
-    log('2. Open pool ledger and get handle from libindy')
-    const poolHandle = await indy.openPoolLedger(poolName, undefined)
+    log('2. Open pool ledger and get handle from libindy');
+    const poolHandle = await indy.openPoolLedger(poolName, undefined);
 
     // 3.
-    log('3. Creating new secure wallet')
-    const walletName = {"id": "wallet"}
-    const walletCredentials = {"key": "wallet_key"}
-    await indy.createWallet(walletName, walletCredentials)
+    log('3. Creating new secure wallet');
+    const walletName = {"id": "wallet"};
+    const walletCredentials = {"key": "wallet_key"};
+    await indy.createWallet(walletName, walletCredentials);
 
     // 4.
-    log('4. Open wallet and get handle from libindy')
-    const walletHandle = await indy.openWallet(walletName, walletCredentials)
+    log('4. Open wallet and get handle from libindy');
+    const walletHandle = await indy.openWallet(walletName, walletCredentials);
 
     // Step 3 code goes here.
 
@@ -62,7 +62,7 @@ async function run() {
 
 
 try {
-    run()
+    run();
 } catch (e) {
-    log("ERROR occured : e")
+    log("ERROR occured : e");
 }
