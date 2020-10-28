91e30cfe854cf5e816b9b6a01da82aa4fb270331
ghabxph <11975597+ghabxph@users.noreply.github.com>
2020-10-28 14:52:27 +0800

    feat(research/node/rotate-key): step 3 is now up and working

--- research/rotate-a-key/src/index.js
+++ research/rotate-a-key/src/index.js
@@ -113,6 +113,23 @@ async function run() {
 
 
     // Step 3 code goes here.
+    // 9.
+    log('9. Generating new verkey of trust anchor in wallet');
+    const newVerkey = await indy.replaceKeysStart(walletHandle, trustAnchorDid, "{}");
+    logValue('New Trust Anchor Verkey: ', newVerkey);
+
+    // 10.
+    log('10. Building NYM request to update new verkey to ledger');
+    const nymRequestForNewVerkey = await indy.buildNymRequest(trustAnchorDid, trustAnchorDid, newVerkey, undefined, 'TRUST_ANCHOR');
+
+    // 11.
+    log('11. Sending NYM request to the ledger');
+    await indy.signAndSubmitRequest(poolHandle, walletHandle, trustAnchorDid, nymRequestForNewVerkey);
+
+    // 12.
+    log('12. Apply new verkey in wallet');
+    await indy.replaceKeysApply(walletHandle, trustAnchorDid);
+
 
     // Step 4 code goes here.
 
