92b0850e031f93ba9ce7c508a6d8cfdab7117c66
ghabxph <11975597+ghabxph@users.noreply.github.com>
2020-10-28 15:35:16 +0800

    feat(research/negotiate-proof): step 3 seems up and running despite step 2 issue

--- research/negotiate-proof/src/index.js
+++ research/negotiate-proof/src/index.js
@@ -97,6 +97,34 @@ async function run() {
 
 
     // Step 3 code goes here.
+    log("9. Prover gets Credentials for Proof Request");
+    const proofRequest = {
+        'nonce': '123432421212',
+        'name': 'proof_req_1',
+        'version': '0.1',
+        'requested_attributes': {
+            'attr1_referent': {
+                'name': 'name',
+                'restrictions': [{
+                    'cred_def_id': credDefId
+                    /*
+                      'issuer_did': issuerDid,
+                      'schema_key': schemaKey
+                    */
+                }]
+            }
+        },
+        'requested_predicates': {
+            'predicate1_referent': {
+                'name': 'age',
+                'p_type': '>=',
+                'p_value': 18,
+                'restrictions': [{'issuer_did': issuerDid}]
+            }
+        }
+    };
+    const credsForProofRequest = await indy.proverGetCredentialsForProofReq(proverWalletHandle, proofRequest);
+
 
     // Step 4 code goes here.
 
